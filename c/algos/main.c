595ed9eca191e01263ae2ed8ae26abd61c2d931f
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-11-21 23:30:37 +0300

    vscode doing its own stuff  Changes to be committed: 	new file:   c/.vscode/tasks.json 	new file:   c/algos/main 	modified:   c/algos/main.c 	modified:   c/out/main

--- c/algos/main.c
+++ c/algos/main.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "01_soru.c"
 #include "02_soru.c"
 #include "03_soru.c"
@@ -19,27 +19,25 @@
 #include "018_rock_paper_scissors.c"
 #include "019_tmp.c"
 
-int main() {
-//	return soru_01();
-//	return soru_02();
-//	return soru_03();
-//	return soru_04();
-//	return soru_05();
-//	return soru_06();
-	return soru_07();
-//	return soru_08();
-//	return soru_09();
-//	return soru_10();
-//	return soru_11();
-//	return soru_12();
-//	return soru_13();
-//	return soru_14();
-//	return soru_015();
-//	return soru_016();
-//	return soru_017();
-//	return soru_018();
-//	return soru_019();
+int main()
+{
+	//	return soru_01();
+	//	return soru_02();
+	//	return soru_03();
+	//	return soru_04();
+	//	return soru_05();
+	//	return soru_06();
+	//	return soru_07();
+	//	return soru_08();
+	//	return soru_09();
+	//	return soru_10();
+	//	return soru_11();
+	//	return soru_12();
+	//	return soru_13();
+	//	return soru_14();
+	//	return soru_015();
+	//	return soru_016();
+	//	return soru_017();
+	return soru_018();
+	//	return soru_019();
 }
-
-
-
