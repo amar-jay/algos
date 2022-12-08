7b8a44debb44a046a2a1a101a98a7b94aa2666c8
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-12-08 23:03:43 +0300

    initialized app

---
+++ go/solution/d0001_integer_partition_test.go
@@ -0,0 +1,26 @@
+package solution
+
+import (
+	"testing"
+
+	"github.com/amar-jay/algos/go/utils"
+)
+
+func test(t *testing.T, n int, res string) bool{
+  sol := Part(n);
+  ans, err := utils.AssertEqual(sol, res);
+  if err != nil {
+    t.Fatalf("Actual: (%s) Expected (%s)", sol, res);
+    return false;
+  }
+  return ans;
+}
+func TestIntegerPartionTestIntegerPartion(t *testing.T) {
+      test(t, 1, "Range: 0 Average: 1.00 Median: 1.00")
+      test(t, 2, "Range: 1 Average: 1.50 Median: 1.50")
+      test(t, 3, "Range: 2 Average: 2.00 Median: 2.00")
+      test(t, 4, "Range: 3 Average: 2.50 Median: 2.50")
+      test(t, 5, "Range: 5 Average: 3.50 Median: 3.50")
+      test(t, 6, "Range: 8 Average: 4.75 Median: 4.50")
+
+}
